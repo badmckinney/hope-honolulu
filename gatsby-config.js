e9b82e95fe8c5969269d08034e8e653955f2ca04
Brad McKinney <brad.mckin@gmail.com>
2019-04-22 19:07:57 -1000

    update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Hope Chapel Honolulu`,
+    description: `Welcome To Our Hope – E Komo Mai I Kou Makou Mana'olana`,
+    author: `@badmckinney`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
